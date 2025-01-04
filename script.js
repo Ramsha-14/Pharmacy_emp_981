62572d893dd850b8bbf06a97bcec35abbd8d2a53
Ramsha-14 <ramshashaikh2514@gmail.com>
2025-01-05 01:20:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// Toggle Menu for Mobile View
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+// Smooth Scrolling for Navigation Links
+const links = document.querySelectorAll('.nav-links a');
+
+links.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const targetId = e.target.getAttribute('href').slice(1);
+        const targetSection = document.getElementById(targetId);
+
+        if (targetSection) {
+            window.scrollTo({
+                top: targetSection.offsetTop - 60,
+                behavior: 'smooth',
+            });
+        }
+    });
+});
